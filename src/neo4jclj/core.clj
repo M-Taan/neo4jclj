0b504c07d7ac842d838aef7a7b0930c527260b8e
Mohamad Taan <mohamadmtaan@gmail.com>
2023-04-26 13:25:11 +0300

    feat: connecting and querying

---
+++ src/neo4jclj/core.clj
@@ -0,0 +1,49 @@
+(ns neo4jclj.core
+  (:require [neo4jclj.parsing :refer [neo4j->clj]])
+  (:import [org.neo4j.driver GraphDatabase AuthTokens Config SessionConfig]
+           [org.neo4j.driver.internal.logging ConsoleLogging]
+           [org.neo4j.driver.internal InternalDriver InternalSession InternalTransaction]
+           [java.util.logging Level]))
+
+(defn- build-config [options]
+  (let [logging (:logging options (ConsoleLogging. Level/CONFIG))]
+    (-> (Config/builder)
+        (.withLogging logging)
+        (.build))))
+
+(defn connect
+  ([url username password]
+   (GraphDatabase/driver url (AuthTokens/basic username password)))
+  ([url username password options]
+   (let [config (build-config options)]
+     (GraphDatabase/driver url (AuthTokens/basic username password) config))))
+
+(defn disconnect [conn]
+  (.close conn))
+
+(defn create-session
+  ([conn]
+   (.session conn))
+  ([conn db]
+   (.session conn (SessionConfig/forDatabase db))))
+
+(defn- query-using-driver
+  [conn q-string param-list]
+  (let [session (create-session conn)]
+    (neo4j->clj (.run session q-string param-list))))
+
+(defn- query-using-session
+  [session q-string param-list]
+  (neo4j->clj (.run session q-string param-list)))
+
+(defn- query-using-transaction [transaction q-string param-list])
+
+(defn query
+  ([obj q-string]
+   (query obj q-string {}))
+  ([obj q-string param-list]
+   (condp = (class obj)
+     InternalDriver (query-using-driver obj q-string param-list)
+     InternalSession (query-using-session obj q-string param-list)
+     InternalTransaction (query-using-transaction obj q-string param-list))))
+
